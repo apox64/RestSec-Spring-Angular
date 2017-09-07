40492cc335ed9f035b1459795308fae0960210cf
apox64 <apox64@web.de>
2017-09-07 11:49:57 +0200

    support for Swagger yaml / yml added

--- backend/src/main/java/de/novatecgmbh/restsecspring/api/crawler/SwaggerCrawlerController.java
+++ backend/src/main/java/de/novatecgmbh/restsecspring/api/crawler/SwaggerCrawlerController.java
@@ -1,7 +1,6 @@
 package de.novatecgmbh.restsecspring.api.crawler;
 
 import de.novatecgmbh.restsecspring.logic.crawler.SwaggerCrawler;
-import de.novatecgmbh.restsecspring.logic.reporting.attackset.Attackset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -20,22 +19,17 @@ public class SwaggerCrawlerController {
     private static Logger logger = LoggerFactory.getLogger(SwaggerCrawlerController.class);
 
     @CrossOrigin
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)//, headers = "content-type=multipart/form-data")
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public String handleFileUpload(@RequestParam ("uploadFile") MultipartFile multipartFile){
-        logger.info("File received! Name: " + multipartFile.getOriginalFilename() + " Size: " + multipartFile.getSize());
         String fileName = multipartFile.getOriginalFilename();
+        logger.info("File received : " + fileName + " (size: " + multipartFile.getSize() + ")");
 
-        Pattern jsonPattern = Pattern.compile(".*json$");
-        Pattern yamlPattern = Pattern.compile(".ya?ml$");
-        Matcher jsonMatcher = jsonPattern.matcher(fileName);
-        Matcher yamlMatcher = yamlPattern.matcher(fileName);
+        Pattern allowedFiles = Pattern.compile("([^\\s]+(\\.(?i)(json|ya?ml))$)");
+        Matcher matcher = allowedFiles.matcher(fileName);
 
-        if (jsonMatcher.find()) {
-            logger.info("Swagger JSON ...");
-        } else if (yamlMatcher.find()) {
-            logger.info("Swagger YAML ...");
-        } else {
+        if (!matcher.find()) {
             logger.warn("Unknown Filetype.");
+            return "Upload failed. Unknown Filetype.";
         }
 
         if (!multipartFile.isEmpty()) {
@@ -45,11 +39,8 @@ public class SwaggerCrawlerController {
                 bos.write(bytes);
                 bos.close();
                 SwaggerCrawler swaggerCrawler = new SwaggerCrawler();
-                swaggerCrawler.crawlJSON(new File("src/main/resources/uploads/"+fileName));
-                logger.info("Sending: " + String.valueOf(swaggerCrawler.getNumberOfEndpoints()));
-//                Attackset attackset = Attackset.getInstance();
-//                return attackset.getAttackSet().toString();
-                return String.valueOf(swaggerCrawler.getNumberOfEndpoints());
+                swaggerCrawler.crawl("src/main/resources/uploads/"+fileName);
+                return "{\"found\":" + String.valueOf(swaggerCrawler.getNumberOfEndpoints() + "}");
             } catch (Exception e) {
                 return "Upload failed. " + fileName + " : " + e.getMessage();
             }
