bc492a55f53e478a4c37c9be8083020f63552b9c
apox64 <apox64@web.de>
2017-09-12 17:16:30 +0200

    init backend results component

---
+++ backend/src/main/java/de/novatecgmbh/restsecspring/logic/reporting/results/Results.java
@@ -0,0 +1,9 @@
+package de.novatecgmbh.restsecspring.logic.reporting.results;
+
+public class Results {
+
+    //TODO: get number of alerts of different levels from zap xml file
+
+    //TODO: offer download of html reports
+
+}
